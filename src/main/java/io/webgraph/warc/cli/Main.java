a498ee4efac3909e3564329673a4ff376f8fd55f
Chris Beams <chris@beams.io>
2017-06-09 17:14:05 +0200

    Add naive 'head' and 'tail' subcommands
    e.g.
    
        $ warc head $WARC_FILE
        $ warc tail $WARC_FILE
    
    `head` is hard-coded to return 10 warc records, and `tail` to return the
    last 3, making it possible to display records 7-10 in $WARC_FILE with
    the following pipeline:
    
        $ warc head $WARC_FILE | warc tail
    
    These hard-coded values are just for illustration. Proper defaults and
    `-n count` options to follow.

--- src/main/java/io/webgraph/warc/cli/Main.java
+++ src/main/java/io/webgraph/warc/cli/Main.java
@@ -1,7 +1,44 @@
 package io.webgraph.warc.cli;
 
+import io.webgraph.warc.Warc;
+import io.webgraph.warc.WarcRecord;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.google.common.collect.EvictingQueue;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String subcommand = args[0];
+
+        InputStream input = args.length > 1 ? new FileInputStream(args[1]) : System.in;
+        OutputStream output = System.out;
+
+        if (subcommand.equals("head"))
+
+            try (Warc.Reader reader = new Warc.Reader(input);
+                 Warc.Writer writer = new Warc.Writer(output)) {
+
+                reader.stream()
+                    .limit(10)
+                    .forEach(writer::write);
+            }
+
+        else if (subcommand.equals("tail"))
+
+            try (Warc.Reader reader = new Warc.Reader(input);
+                 Warc.Writer writer = new Warc.Writer(output)) {
+
+                EvictingQueue<WarcRecord> tail = EvictingQueue.create(3);
+
+                reader.forEach(tail::add);
+                tail.forEach(writer::write);
+            }
+
+        else throw new IllegalArgumentException("unknown subcommand: " + subcommand);
     }
 }
